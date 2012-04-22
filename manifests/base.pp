8f8d99b495f880f6b94135b0b6a97929318f50da
Daniel Zuberbühler <dani@zubinet.org>
2012-04-22 22:48:33 +0200

    add init script/config to autostart rtorrent after boot as user rtorrent

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,4 +2,46 @@ class rtorrent::base {
   package { 'rtorrent':
     ensure => present,
   }    
+
+  file{'rtorrent-init':
+    path    => "/etc/init.d/rtorrent",
+    source  => [ "puppet:///modules/site-rtorrent/init.d/rtorrent",
+                "puppet:///modules/rtorrent/init.d/rtorrent" ],
+    require => Package['rtorrent'],
+    owner   => root, group => 0, mode => 0755;
+  }
+
+  user{'rtorrent':
+    ensure     => present,
+    managehome => true,
+    home       => '/home/rtorrent',
+  }
+
+  file{'rtorrent.rc':
+    path    => "/home/rtorrent/.rtorrent.rc",
+    source  => [ "puppet:///modules/site-rtorrent/rtorrent.rc",
+                 "puppet:///modules/rtorrent/rtorrent.rc" ],
+    ensure  => present,
+    require => User['rtorrent'],
+  }
+  
+  file{'session-dir':
+    path    => "/home/rtorrent/session",
+    ensure  => directory, 
+    owner   => 'rtorrent',
+    group   => 'rtorrent',
+    require => User['rtorrent'],
+  }
+
+  service{'rtorrent':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => [ File['/etc/init.d/rtorrent'], 
+                   User['rtorrent'],
+                   File['rtorrent.rc'],
+                   File['session-dir']
+                 ],
+  }
+
 }
